e70ff2f1e47375819d400c7c4449fe44f1d512b2
Marian Longa <marian.longa@gmail.com>
2017-03-19 14:40:47 +0000

    [ML] added aliens with collision script, updated level

--- Fizzyo-Chromosome7/Assets/Scripts/DestroyOnCollision.cs
+++ Fizzyo-Chromosome7/Assets/Scripts/DestroyOnCollision.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DestroyOnCollision : MonoBehaviour {
 
-	void OnTriggerEnter(Collider other) {
+	void OnTriggerEnter2D(Collider2D other) {
 		Destroy (other.gameObject);
 		Destroy (gameObject);
 	}
