97202c50139122f3376d29969400878bf6e6efbf
Marian Longa <marian.longa@gmail.com>
2017-03-19 12:50:55 +0000

    [ML] added Player controller to move player up/down, removed old player controller

--- Fizzyo-Chromosome7/Assets/Scripts/Player.cs
+++ Fizzyo-Chromosome7/Assets/Scripts/Player.cs
@@ -1,73 +1,46 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class Player : MonoBehaviour
-{
+public class Player : MonoBehaviour {
 
-    //public
-    public float maxHeight = 5f;
-    public bool smoothMovement = true;
-    public float maxFizzyoPressure = 0.5f; //calibrated on start screen
-    public GameObject missilePrefab;
+	private float newY;
+	public float speed;
+	public float maxY;
+	public float minY;
+	public float deltaT;
+	private float nextT;
+	private bool movingDown;
 
-    //private
-    float destXSpeed = 0.02f;
-    private float smoothing = 0.03f;
-    private float xSpeed = 0f;
+	// Use this for initialization
+	void Start () {
+		newY = 0;
+		speed = 0.1f;
+		maxY = 4f;
+		minY = -4f;
+		deltaT = 0.01f;
+		nextT = Time.time;
+		movingDown = true;
+		transform.position = new Vector3 (-9, transform.position.x, transform.position.z);
+	}
 
-    // Use this for initialization
-    void Start()
-    {
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > nextT) {
+			if (movingDown)
+				newY = transform.position.y - speed;
+			else
+				newY = transform.position.y + speed;
+		
+			transform.position = new Vector3 (transform.position.x, newY, transform.position.z);
 
-        //load our sensor calibration if already set
-        if (PlayerPrefs.HasKey("Max Fizzyo Pressure"))
-        {
-            maxFizzyoPressure = PlayerPrefs.GetFloat("Max Fizzyo Pressure");
-            Debug.Log("Set max fizzyo pressure val to: " + maxFizzyoPressure);
-        }
+			if (newY < minY)
+				movingDown = false;
+			if (newY > maxY)
+				movingDown = true;
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //get the pressure value of our fizzyo device communicated as a joystick axis 0-1 blow out -1-0 breath in
-        //float fizzyoVal = Input.GetAxisRaw("Horizontal");
-
-        //get the pressure value from our fizzyo device or logged pressure data if useRecordedData is set to true.
-        float pressure = FizzyoDevice.Instance().Pressure();
-
-        float destHeight = maxHeight * Mathf.Min((pressure / maxFizzyoPressure), 1);
-
-        float y;
-
-        if (smoothMovement)
-        {
-            y = transform.position.y + ((destHeight - transform.position.y) * smoothing);
-        }
-        else
-        {
-            y = destHeight;
-        }
-
-        float x = transform.position.x;
-        if (y > 0.15f)
-        {
-            xSpeed += (destXSpeed - xSpeed) * smoothing;
-            x += xSpeed;
-        }
-        else
-        {
-            xSpeed = 0;
-        }
-        transform.position = new Vector3(x, y, transform.position.z);
-
-        if (FizzyoDevice.Instance().ButtonDown() || Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Fire1"))
-        {
-            var pos = transform.position;
-            pos.y += 0.5f;
-            Instantiate(missilePrefab, pos, transform.rotation);
-        }
-    }
-
+			nextT = Time.time + deltaT;
+		}
+	}
 }
+	
