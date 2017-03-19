9a0db2d3ed3a239816589572f88aed79f9db448b
Rares-Andrei Dorcioman <rad216@ic.ac.uk>
2017-03-19 12:22:01 +0000

    [RAD] Added function for continuous fire

--- Fizzyo-Chromosome7/Assets/Scripts/ShootController.cs
+++ Fizzyo-Chromosome7/Assets/Scripts/ShootController.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class ShootController : MonoBehaviour {
     public GameObject missilePrefab;
+    public float fireRate;
+    private float nextFire;
 
-	// Use this for initialization
-	void Start () {
-		
+    // Use this for initialization
+    void Start () {
+        fireRate = 0.2f;
+        nextFire = 0.01f;
 	}
 	
 	// Update is called once per frame
@@ -15,6 +18,12 @@ public class ShootController : MonoBehaviour {
         if (FizzyoDevice.Instance().ButtonDown() || Input.GetKeyDown(KeyCode.Space))
         {
            GameObject missile = (GameObject)Instantiate(missilePrefab, transform.position, transform.rotation);
+        }
+        else if ((FizzyoDevice.Instance().ButtonDown() || Input.GetKey(KeyCode.Space)) && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            GameObject missile = (GameObject)Instantiate(missilePrefab, transform.position, transform.rotation);
+            //Instantiate(missilePrefab, transform.position, transform.rotation);
         }
 
     }
