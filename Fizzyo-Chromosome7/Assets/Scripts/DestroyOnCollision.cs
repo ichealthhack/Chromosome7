15dd8ead0c9896d7c43846d4adcf58bd17a201b3
Marian Longa <marian.longa@gmail.com>
2017-03-19 13:38:48 +0000

    [ML] added Asteroid class and collisions script

---
+++ Fizzyo-Chromosome7/Assets/Scripts/DestroyOnCollision.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnCollision : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		Destroy (other.gameObject);
+		Destroy (gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
