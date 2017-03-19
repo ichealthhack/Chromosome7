fc96b6c75d962fbba9fca447a868ecf7bb78f228
Rares-Andrei Dorcioman <rad216@ic.ac.uk>
2017-03-19 11:04:49 +0000

    Add missile controller

---
+++ Fizzyo-Chromosome7/Assets/Scripts/ShootController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootController : MonoBehaviour {
+    public GameObject missilePrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+           GameObject missile = (GameObject)Instantiate(missilePrefab, transform.position, transform.rotation);
+        }
+
+    }
+}
